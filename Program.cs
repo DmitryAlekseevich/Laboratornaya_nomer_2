ef3c128eaa229c87a3155d38e037f0481a803563
DmitryAlekseevich <113106163+DmitryAlekseevich@users.noreply.github.com>
2022-10-06 20:40:46 +0300

    Начал писать класс Person
    Добавил поля string с именем, фамилией и DateTime

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine();
+            ResearchTeam std = new ResearchTeam(); //Один объект типа ResearchTeam
+            Console.WriteLine(std.ToShortString()); //Преобразовать данные в текстовый вид с помощью метода ToShortString() и вывести данные
+        }
+    }
+
+    class Person
+    {
+        private
+
+        string Name; //закрытое поле string. имя
+        string LastName; //закрытое поле string. фамилия
+        System.DateTime BDate;//закрытое поле System.DateTime. дата рождения
+
+
+        
+    }
+}
